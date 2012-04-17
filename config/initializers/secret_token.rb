5cdbd5d03daea5e534df8e4dde6c296dbfc0d20c
Gino Garcia <ginogarcia@netzero.net>
2012-04-17 15:40:37 -0500

    untracked files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChapterTen::Application.config.secret_token = '0f875b1ad79516b9487dd3519625dd447bb24762f3e570e0f3c7a7a97e9ad52cfc649eac7d235a2933a9629bd748ea67783a431bae96bd91811d5de25bc6d864'
