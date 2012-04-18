1de50b49daba5c356fce01a6df95148d978dd05e
Gino Garcia <ginogarcia@netzero.net>
2012-04-18 18:55:17 -0500

    broken admin field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120418083956) do
+ActiveRecord::Schema.define(:version => 20120418234708) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
@@ -19,7 +19,6 @@ ActiveRecord::Schema.define(:version => 20120418083956) do
     t.datetime "updated_at"
     t.string   "encrypted_password"
     t.string   "salt"
-    
   end
 
 end
