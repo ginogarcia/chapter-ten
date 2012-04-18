4f8faa617ee3093d89b82fc9d95891b30403175b
Gino Garcia <ginogarcia@netzero.net>
2012-04-18 03:19:53 -0500

    adding paginate

--- Gemfile
+++ Gemfile
@@ -1,23 +1,21 @@
 source 'http://rubygems.org'
 
 gem 'rails', '3.0.4'
-
-# Bundle edge Rails instead:
-# gem 'rails', :git => 'git://github.com/rails/rails.git'
-
 gem 'sqlite3'
 gem 'gravatar_image_tag', '0.1.0'
+gem 'will_paginate', 	'3.0.pre'
 
 
 
 
 group :development do
-   gem 'rspec-rails', '2.8.0'
+   gem 'rspec-rails', 	'2.8.0'
    gem 'annotate',      '2.4.0'
+   gem 'faker', 	'0.3.1'
  end
  
  group :test do
-   gem 'rspec-rails', '2.8.0'
+   gem 'rspec-rails', 	'2.8.0'
    gem 'webrat',        '0.7.1'
    gem 'annotate',      '2.4.0'
    gem 'factory_girl_rails', '1.0'
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,8 +10,6 @@ describe UsersController do
             it "should deny access" do
                 get :index
                 response.should redirect_to(signin_path)
-               
-                
             end
         end
         ###
@@ -20,7 +18,11 @@ describe UsersController do
             before(:each) do
                 @user = test_sign_in(Factory(:user))
                 Factory(:user, :email => "another@example.com")
-                 Factory(:user, :email => "another@example.net")
+                Factory(:user, :email => "another@example.net")
+                
+                30.times do
+                    Factory(:user, :email => Factory.next(:email))
+                end
             end
             ###
             it "should be successfull" do
@@ -34,13 +36,21 @@ describe UsersController do
             end
             it "should have and element for each user" do
                 get :index
-                User.all.each do |user|
+                User.paginate(:page => 1).each do |user|
                     response.should have_selector("li", :content => user.name)
                 end
             end
+            ###
+            it "should paginate users" do
+                get :index
+                response.should have_selector("div.pagination")
+                response.should have_selector("span.disabled", :content => "Previous")
+                response.should have_selector("a", :href => "/users?page=2",
+                                                   :content => '2')
+                response.should have_selector("a", :href => "/users?page=2",
+                                                   :content => "Next")
+            end
         end
-       
-        
     end
     ####
     describe "GET 'show'" do
