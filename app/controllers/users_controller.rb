39e77a18831f1ed3feeaeb0f904c3f0fc24f868a
Gino Garcia <ginogarcia@netzero.net>
2012-04-18 01:14:59 -0500

    added security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_filter :authenticate, :only => [:edit, :update]
+    before_filter :correct_user, :only => [:edit, :update]
+
   def show
     @user  = User.find(params[:id])
     @title = @user.name 
@@ -21,6 +23,7 @@ class UsersController < ApplicationController
   end
   ###
   def edit
+   ## raise request.inspect
     @user  = User.find(params[:id])
     @title = "Edit user"
   end
@@ -36,9 +39,14 @@ class UsersController < ApplicationController
   end
   ###
   
-   private
+  private
     def authenticate
         deny_access unless signed_in?
     end
     ###
+    def correct_user
+      @user =User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+    
 end
